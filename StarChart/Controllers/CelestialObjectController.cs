a5286946e103c822e482ebff26da318ef7de66bd
Richard Morrison <mrfelis@yahoo.com>
2022-11-14 13:12:19 -0500

    step 5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +14,43 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(o => o.Id == id);
+
+            if (item == null) { return NotFound(); }
+
+            SetSatellites(item);
+            return Ok(item);
+        }
+
+        private void SetSatellites(CelestialObject item)
+        {
+            item.Satellites = _context.CelestialObjects.Where(o => o.OrbitedObjectId == item.Id).ToList();
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var items = _context.CelestialObjects.Where(o => o.Name == name).ToList();
+
+            if (!items.Any()) { return NotFound(); }
+
+            items.ForEach(SetSatellites);
+
+            return Ok(items);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var items = _context.CelestialObjects.ToList();
+            items.ForEach(SetSatellites);
+            
+            return base.Ok(items);
         }
     }
 }
