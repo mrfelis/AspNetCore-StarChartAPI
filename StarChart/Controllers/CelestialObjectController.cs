7a9c2067a60c7991510743cc8d913399880e88c9
Richard Morrison <mrfelis@yahoo.com>
2022-12-06 20:36:11 -0500

    step 6

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 using StarChart.Models;
@@ -51,6 +53,60 @@ namespace StarChart.Controllers
             items.ForEach(SetSatellites);
             
             return base.Ok(items);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CelestialObject co)
+        {
+            _context.CelestialObjects.Add(co);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetById", new { Id = co.Id }, co);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject co)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(o => o.Id == id);
+
+            if (item == null) { return NotFound(); }
+
+            item.Name = co.Name;
+            item.OrbitalPeriod = co.OrbitalPeriod;
+            item.OrbitedObjectId = co.OrbitedObjectId;
+
+            _context.Update(item);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(o => o.Id == id);
+
+            if (item == null) { return NotFound(); }
+
+            item.Name = name;
+
+            _context.Update(item);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(o => o.Id == id);
+
+            if (item == null) { return NotFound(); }
+
+            _context.RemoveRange(item); 
+            _context.SaveChanges();
+
+            return NoContent(); 
         }
     }
 }
